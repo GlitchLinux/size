c9f07eab920ac622a6293ccc80db56031fbfad14
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2026-02-08 05:46:08 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,6 +5,7 @@ sudo rm -f size
 sudo wget https://raw.githubusercontent.com/GlitchLinux/size/refs/heads/main/size 
 sudo chmod +x size
 sudo chmod 777 size
+clear
 echo ""
 echo "size Utility Installed to system"
 echo ""
